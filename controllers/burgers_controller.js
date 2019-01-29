ac7f7a8dc08a0cb09675c70d91934a6dc86efe71
ianbunn <ianrichardbunn@gmail.com>
2019-01-28 21:57:11 -0600

    completed insert and updated calls to DB using ORM and model based on controller routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,5 +15,31 @@ router.get("/",(request,response)=>{
     })
 });
 
+
+router.post('/api/burgers',(request,response)=>{
+    burger.insertOne(['burger_name','devoured'],
+    [request.body.burger_name, request.body.devoured], (result)=>{
+        response.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/burgers/:id",(request,response)=>{
+    var condition = `ID = ${request.params.id}`;
+    
+    burger.updateOne({
+        devoured: request.body.devoured
+    },
+    condition,
+    (result)=>{
+        if(result.changedRows === 0){
+            return response.status(404).end();
+        };
+        
+        response.status(200).end();
+    });
+});
+
 // EXPORT ROUTES FOR 'server.js'
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -6,8 +6,18 @@ var burger = {
         orm.selectAll('burgers',(response)=>{
             callback(response);
         })
+    },
+    insertOne: (columns,values,callback)=>{
+        orm.insertOne("burgers",columns,values,(response=>{
+            callback(response);
+        }));
+    },
+    updateOne: (objectColumnValues,condition,callback)=>{
+        orm.updateOne("burgers",objectColumnValues,condition,(response)=>{
+            callback(response);
+        });
     }
-}
+};
 
 // EXPORT DB FUNCTIONS FOR CONTROLLER (burgers_controller.js)
 module.exports = burger;
